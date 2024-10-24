23fe1f006e54d0cbe64f2fef3494a56f1d3356b8
phelan <phelan.minhtanly@gmail.com>
2024-10-24 11:19:59 +0700

    change route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,8 @@ const SingerRouter = require('./singer-router');
 
 function route(app) {
 	app.use('/user', userRouter);
-	app.use('/genre', GenreRouter);
-	app.use('/topic', TopicRouter);
+	app.use('/browse/genre', GenreRouter);
+	app.use('/browse/topic', TopicRouter);
 	app.use('/singer', SingerRouter);
 }
 
