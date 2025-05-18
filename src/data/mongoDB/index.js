4606ef2883fbb8511a18edec9192c55059393e3a
phelan <phelan.minhtanly@gmail.com>
2025-05-18 18:46:01 +0700

    deleted variable

--- src/data/mongoDB/index.js
+++ src/data/mongoDB/index.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const config = require('../../config');
 
 async function connect() {
+	console.log(
+		`mongodb+srv://${config.development.db.username}:${config.development.db.password}@music.8qvgt.mongodb.net/cohesive?retryWrites=true&w=majority&appName=music`,
+	);
 	await mongoose
 		.connect(
 			`mongodb+srv://${config.development.db.username}:${config.development.db.password}@music.8qvgt.mongodb.net/cohesive?retryWrites=true&w=majority&appName=music`,
