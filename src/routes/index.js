d6fc39930724bed9ac6503e6de172aaffc5d8e55
phelan <phelan.minhtanly@gmail.com>
2025-05-18 18:51:41 +0700

    open routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,16 +7,13 @@ const PlaylistRouter = require('./playlist-router');
 const OtherRouter = require('./other-router');
 
 function route(app) {
-	// app.use('/user', userRouter);
-	// app.use('/browse/genre', GenreRouter);
-	// app.use('/browse/topic', TopicRouter);
-	// app.use('/singer', SingerRouter);
-	// app.use('/song', SongRouter);
-	// app.use('/playlist', PlaylistRouter);
-	// app.use('/other', OtherRouter);
-	  app.get('/', (req, res) => {
-			res.send('Hello from root');
-		});
+	app.use('/user', userRouter);
+	app.use('/browse/genre', GenreRouter);
+	app.use('/browse/topic', TopicRouter);
+	app.use('/singer', SingerRouter);
+	app.use('/song', SongRouter);
+	app.use('/playlist', PlaylistRouter);
+	app.use('/other', OtherRouter);
 }
 
 module.exports = route;
